215b8f0a9f8bb017fc6e79e7a20eeae7f9b80b1c
mmdcloud <madmaxcloudpractice@gmail.com>
2025-06-09 17:23:11 +0530

    update: added comput env, job queue and job definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,109 +1,29 @@
-# IAM Role for AWS Batch Service
-resource "aws_iam_role" "batch_service_role" {
-  name = "aws_batch_service_role"
+data "aws_iam_policy_document" "batch_assume_role" {
+  statement {
+    effect = "Allow"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "batch.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# IAM Role for ECS Tasks
-resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs_task_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Batch Compute Environment
-resource "aws_batch_compute_environment" "batch_env" {
-  compute_environment_name = "my-batch-compute-env"
-
-  compute_resources {
-    max_vcpus = 16
-    min_vcpus = 0
-
-    security_group_ids = [aws_security_group.batch_sg.id]
-    subnets           = [aws_subnet.batch_subnet.id]
-    type             = "FARGATE" # Or "EC2" for EC2 launch type
-
-    instance_role = aws_iam_role.ecs_task_role.arn
-  }
-
-  service_role = aws_iam_role.batch_service_role.arn
-  type         = "MANAGED"
-  depends_on   = [aws_iam_role_policy_attachment.batch_service_role]
-}
-
-# Batch Job Queue
-resource "aws_batch_job_queue" "batch_queue" {
-  name     = "my-batch-job-queue"
-  state    = "ENABLED"
-  priority = 1 
-  compute_environments = [aws_batch_compute_environment.batch_env.arn]
-}
-
-# Simple Batch Job Definition
-resource "aws_batch_job_definition" "example_job" {
-  name = "example-job-definition"
-  type = "container"
-
-  container_properties = jsonencode({
-    command    = ["echo", "Hello from AWS Batch"]
-    image      = "public.ecr.aws/amazonlinux/amazonlinux:latest"
-    jobRoleArn = aws_iam_role.ecs_task_role.arn
-    fargatePlatformConfiguration = {
-      platformVersion = "LATEST"
+    principals {
+      type        = "Service"
+      identifiers = ["batch.amazonaws.com"]
     }
-    resourceRequirements = [
-      {
-        type  = "VCPU"
-        value = "1"
-      },
-      {
-        type  = "MEMORY"
-        value = "2048"
-      }
-    ]
-    executionRoleArn = aws_iam_role.ecs_task_role.arn
-  })
 
-  platform_capabilities = ["FARGATE"]
+    actions = ["sts:AssumeRole"]
+  }
 }
 
-# Supporting Network Resources (minimal setup)
-resource "aws_vpc" "batch_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_iam_role" "aws_batch_service_role" {
+  name               = "aws_batch_service_role"
+  assume_role_policy = data.aws_iam_policy_document.batch_assume_role.json
 }
 
-resource "aws_subnet" "batch_subnet" {
-  vpc_id     = aws_vpc.batch_vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
+  role       = aws_iam_role.aws_batch_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
-resource "aws_security_group" "batch_sg" {
-  name   = "batch-security-group"
-  vpc_id = aws_vpc.batch_vpc.id
-
+resource "aws_security_group" "sample" {
+  name = "aws_batch_compute_environment_security_group"
+  vpc_id = aws_vpc.sample.id
   egress {
     from_port   = 0
     to_port     = 0
@@ -112,8 +32,96 @@ resource "aws_security_group" "batch_sg" {
   }
 }
 
-# IAM Policy Attachment
-resource "aws_iam_role_policy_attachment" "batch_service_role" {
-  role       = aws_iam_role.batch_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+resource "aws_vpc" "sample" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_subnet" "sample" {
+  vpc_id     = aws_vpc.sample.id
+  cidr_block = "10.1.1.0/24"
+}
+
+
+resource "aws_batch_compute_environment" "sample" {
+  compute_environment_name = "sample"
+  compute_resources {
+    max_vcpus = 16
+
+    security_group_ids = [
+      aws_security_group.sample.id
+    ]
+
+    subnets = [
+      aws_subnet.sample.id
+    ]
+
+    type = "FARGATE"
+  }
+
+  service_role = aws_iam_role.aws_batch_service_role.arn
+  type         = "MANAGED"
+  depends_on   = [aws_iam_role_policy_attachment.aws_batch_service_role]
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "tf_test_batch_exec_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_batch_job_definition" "test" {
+  name = "tf_test_batch_job_definition"
+  type = "container"
+
+  platform_capabilities = [
+    "FARGATE",
+  ]
+
+  container_properties = jsonencode({
+    command = ["echo", "test"]
+    image   = "busybox"
+
+    fargatePlatformConfiguration = {
+      platformVersion = "LATEST"
+    }
+
+    resourceRequirements = [
+      {
+        type  = "VCPU"
+        value = "0.25"
+      },
+      {
+        type  = "MEMORY"
+        value = "512"
+      }
+    ]
+
+    executionRoleArn = aws_iam_role.ecs_task_execution_role.arn
+  })
+}
+
+resource "aws_batch_job_queue" "test_queue" {
+  name     = "tf-test-batch-job-queue"
+  state    = "ENABLED"
+  priority = 1
+
+  compute_environment_order {
+    order               = 1
+    compute_environment = aws_batch_compute_environment.sample.arn
+  }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "job_queue_arn" {
-  value = aws_batch_job_queue.batch_queue.arn
+  value = aws_batch_job_queue.test_queue.arn
 }
 
 output "job_definition_arn" {
-  value = aws_batch_job_definition.example_job.arn
+  value = aws_batch_job_definition.test.arn
 }
