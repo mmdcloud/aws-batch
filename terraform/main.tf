0b7fcd3d0ac32561b513fbfe2887fc52aca94ac6
mmdcloud <madmaxcloudpractice@gmail.com>
2025-06-17 16:54:30 +0530

    update: added ecr and vpc module

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,38 +21,130 @@ resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
-resource "aws_security_group" "sample" {
-  name = "aws_batch_compute_environment_security_group"
-  vpc_id = aws_vpc.sample.id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# VPC Configuration
+module "vpc" {
+  source                = "./modules/vpc/vpc"
+  vpc_name              = "vpc"
+  vpc_cidr_block        = "10.0.0.0/16"
+  enable_dns_hostnames  = true
+  enable_dns_support    = true
+  internet_gateway_name = "vpc_igw"
 }
 
-resource "aws_vpc" "sample" {
-  cidr_block = "10.1.0.0/16"
+# Security Group
+module "security_group" {
+  source = "./modules/vpc/security_groups"
+  vpc_id = module.vpc.vpc_id
+  name   = "security-group"
+  ingress = [
+    {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "tcp"
+      self            = "false"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+      description     = "any"
+    }
+  ]
+  egress = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 }
 
-resource "aws_subnet" "sample" {
-  vpc_id     = aws_vpc.sample.id
-  cidr_block = "10.1.1.0/24"
+# Public Subnets
+module "public_subnets" {
+  source = "./modules/vpc/subnets"
+  name   = "public-subnet"
+  subnets = [
+    {
+      subnet = "10.0.1.0/24"
+      az     = "us-east-1a"
+    },
+    {
+      subnet = "10.0.2.0/24"
+      az     = "us-east-1b"
+    },
+    {
+      subnet = "10.0.3.0/24"
+      az     = "us-east-1c"
+    }
+  ]
+  vpc_id                  = module.vpc.vpc_id
+  map_public_ip_on_launch = true
 }
 
+# Private Subnets
+module "private_subnets" {
+  source = "./modules/vpc/subnets"
+  name   = "private-subnet"
+  subnets = [
+    {
+      subnet = "10.0.6.0/24"
+      az     = "us-east-1c"
+    },
+    {
+      subnet = "10.0.5.0/24"
+      az     = "us-east-1b"
+    },
+    {
+      subnet = "10.0.4.0/24"
+      az     = "us-east-1a"
+    }
+  ]
+  vpc_id                  = module.vpc.vpc_id
+  map_public_ip_on_launch = false
+}
 
-resource "aws_batch_compute_environment" "sample" {
-  compute_environment_name = "sample"
+# Public Route Table
+module "public_rt" {
+  source  = "./modules/vpc/route_tables"
+  name    = "public-route-table"
+  subnets = module.public_subnets.subnets[*]
+  routes = [
+    {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = ""
+      gateway_id     = module.vpc.igw_id
+    }
+  ]
+  vpc_id = module.vpc.vpc_id
+}
+
+# Private Route Table
+module "private_rt" {
+  source  = "./modules/vpc/route_tables"
+  name    = "private-route-table"
+  subnets = module.private_subnets.subnets[*]
+  routes  = []
+  vpc_id  = module.vpc.vpc_id
+}
+
+module "ecr" {
+  source               = "./modules/ecr"
+  force_delete         = true
+  scan_on_push         = false
+  image_tag_mutability = "IMMUTABLE"
+  bash_command         = ""
+  name                 = "batch_job"
+}
+
+resource "aws_batch_compute_environment" "batch_compute" {
+  compute_environment_name = "batch-compute"
   compute_resources {
     max_vcpus = 16
 
     security_group_ids = [
-      aws_security_group.sample.id
+      module.security_group.id
     ]
 
     subnets = [
-      aws_subnet.sample.id
+      module.public_subnets.subnets[0].id
     ]
 
     type = "FARGATE"
@@ -84,44 +176,60 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_batch_job_definition" "test" {
-  name = "tf_test_batch_job_definition"
+resource "aws_iam_role_policy_attachment" "ecr_registry_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_cloudwatch_logs" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_batch_job_definition" "batch_job_definition" {
+  name = "batch-job-definition"
   type = "container"
 
-  platform_capabilities = [
-    "FARGATE",
-  ]
+  platform_capabilities = ["FARGATE"]
 
   container_properties = jsonencode({
-    command = ["echo", "test"]
-    image   = "busybox"
+    command    = ["echo", "Hello AWS Batch"],
+    image      = "busybox",
+    jobRoleArn = aws_iam_role.ecs_task_execution_role.arn,
 
     fargatePlatformConfiguration = {
       platformVersion = "LATEST"
-    }
+    },
 
     resourceRequirements = [
-      {
-        type  = "VCPU"
-        value = "0.25"
-      },
-      {
-        type  = "MEMORY"
-        value = "512"
-      }
-    ]
+      { type = "VCPU", value = "0.25" },
+      { type = "MEMORY", value = "512" }
+    ],
 
-    executionRoleArn = aws_iam_role.ecs_task_execution_role.arn
+    executionRoleArn = aws_iam_role.ecs_task_execution_role.arn,
+
+    networkConfiguration = {
+      assignPublicIp = "ENABLED"
+    },
+
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        "awslogs-group"         = "/aws/batch/job",
+        "awslogs-region"        = "us-east-1",
+        "awslogs-stream-prefix" = "ecs"
+      }
+    }
   })
 }
 
-resource "aws_batch_job_queue" "test_queue" {
-  name     = "tf-test-batch-job-queue"
+resource "aws_batch_job_queue" "batch_job_queue" {
+  name     = "batch-job-queue"
   state    = "ENABLED"
   priority = 1
 
   compute_environment_order {
     order               = 1
-    compute_environment = aws_batch_compute_environment.sample.arn
+    compute_environment = aws_batch_compute_environment.batch_compute.arn
   }
 }
